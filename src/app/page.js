b24ab0943959ea948fecfbc7c1c78e00ae6e4bd4
Juan Manuel Sandoval Olavarría <correo.juanm.sandoval@gmail.com>
2023-12-06 18:51:52 -0300

    Se arregla la instalación de next-ui

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,15 @@
 "use client";
-import { useState } from 'react';
+import {Button} from '@nextui-org/button'; 
 
-function Botonsito() {
-  return(
-    <button className='border-2 border-gray-600 hover:bg-gray-400 active:m-2'>xd</button>
-  )
-}
 
 export default function Home() {
-  const [num, setNum] = useState(0);
-  const arregloComponents = [];
-  for (let i = 0; i < num; i++) {
-    arregloComponents.push(
-      <Botonsito key={i}/>
-    )
-  }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className='text-black flex flex-col'>
-        SCIBLOG DONENOS EN CUENTA RUT 20626454-3 xd
+    <main className=" flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <Button color="primary">
+          Button
+        </Button>
       </div>
     </main>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,12 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-    plugins: [nextui()],
+    extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  
+  plugins: [nextui()]
 }
