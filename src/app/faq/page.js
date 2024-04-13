84e99a3c3f3b37830c809df1bbad1fd393218416
Ignacio-Ruiz <ignacioeladio.ruiz@alumnos.ulagos>
2024-04-13 17:56:23 -0400

    UPDATE 13/0472024
    Se realizan algunos cambios en los textos y se continua con la creacion de tablas para la base de datos

--- src/app/faq/page.js
+++ src/app/faq/page.js
@@ -1,24 +1,24 @@
 
 export default function Page() {
   return (
-    <div className="bg-white p-8 w-full">
+    <div className="bg-purple p-8 w-full">
       <h1 className="text-5xl font-bold mb-6">Preguntas frecuentes</h1>
       <div className="space-y-4 w-full">
-        <details className="p-4 bg-gray-200 rounded w-full">
+        <details className="p-4 bg-purple-200 rounded w-full">
           <summary className="text-xl font-semibold cursor-pointer w-full">¿Como puedo comentar una publicacion?</summary>
           <p className="pt-2 w-full">
             Se puede comentar una publicacion seleccionando la publicacion, dirigiendose hacia las opciones de abajo y
             seleccionar la segunda opcion(la derecha del corazon).
           </p>
         </details>
-        <details className="p-4 bg-gray-200 rounded">
+        <details className="p-4 bg-purple-200 rounded">
           <summary className="text-xl font-semibold cursor-pointer">¿Como puedo unirme a un grupo?</summary>
           <p className="pt-2">
             Para unirse a un grupo, se tiene que dirigir al apartado de comunidad, seleccionar un grupo, y luego hacer
             click al boton de unirse.
           </p>
         </details>
-        <details className="p-4 bg-gray-200 rounded">
+        <details className="p-4 bg-purple-200 rounded">
           <summary className="text-xl font-semibold cursor-pointer">¿Como puedo crear una publicacion?</summary>
           <p className="pt-2">
             Para crear un publicacion se tiene que estar en el inicio de la red social, puede ir al inicio haciendo
@@ -26,7 +26,7 @@ export default function Page() {
             los datos que se soliciten.
           </p>
         </details>
-        <details className="p-4 bg-gray-200 rounded">
+        <details className="p-4 bg-purple-200 rounded">
           <summary className="text-xl font-semibold cursor-pointer">¿Como puedo cerrar sesion?</summary>
           <p className="pt-2">
             para cerrar sesion solo tienes que dirigirte a las opciones de la izquierda ,abajo , y hacer click en cerrar
--- src/components/PostCarousel.js
+++ src/components/PostCarousel.js
@@ -9,16 +9,16 @@ const PostCarousel = ({className}) => {
         <div className="relative overflow-x-hidden w-4/5 ">
           <div className="flex -mx-4 snap-x snap-mandatory overflow-x-scroll scrollbar-hide">
             <div className="w-1/3 px-4 flex-shrink-0 snap-center">
-              <MiniPost title="Cancer" description="Mucho cancer en chile"/>
+              <MiniPost title={<strong>Tendencias en Uso de Tecnología Wearable en el Área de la Salud:</strong>} description=" Las tendencias emergentes en el uso de tecnología wearable para el monitoreo de la salud"/>
             </div>  
             <div className="w-1/3 px-4 flex-shrink-0 snap-center">
-              <MiniPost title="Diabetes en watones" description="No comas tanto amigo, deja los azucares"/>
+              <MiniPost title={<strong>Desafíos y Oportunidades en Salud Mental Infantil</strong>} description=" Desafíos actuales en el diagnóstico, tratamiento y prevención de trastornos de salud mental en niños y adolescentes"/>
             </div>
             <div className="w-1/3 px-4 flex-shrink-0 snap-center">
-              <MiniPost title="Calambre ahhh" description="Como evitar los calambritos"/>
+              <MiniPost title={<strong>Impacto de la Telemedicina en la Atención Primaria de Salud:</strong>} description=" Este informe analiza cómo la implementación de la telemedicina ha afectado la accesibilidad, eficacia y calidad de la atención primaria de salud"/>
             </div>
             <div className="w-1/3 px-4 flex-shrink-0 snap-center">
-              <MiniPost title="Muerte subdita en chile" description="Hay gente que se muere porque si"/>
+              <MiniPost title={<strong>Evaluación de la Efectividad de Programas de Vacunación:</strong>} description=" Evalúa la efectividad de los programas de vacunación en diferentes regiones, examinando tasas de cobertura, incidencia de enfermedades prevenibles por vacunación"/>
             </div>
           </div>  
         </div>
