9a1ff339216a6e293044aa9cee79b07e5986ee1b
sebaurrutia <sebastianandres.urrutia@alumnos.ulagos.cl>
2024-05-24 18:22:52 -0400

    comunidad grupos

--- src/app/comunidad/page.js
+++ src/app/comunidad/page.js
@@ -7,21 +7,22 @@ export default function Page() {
     return (
         <div className='flex flex-col w-full'>
 
-             <div class="flex w-full flex-row mx-5  h-full ">
+            <div class="flex w-full flex-row mx-5 ">
                 <div className="flex flex-col w-full my-6 mx-5 px-2 space-y-4">
                     <h1 className='font-semibold text-3xl'>Comunidad</h1>
 
                     <div className='flex flex-row w-full'>
                         <SearchBar className='w-full' placeholder="Buscar Grupo"/>
                         <div className='flex flex-row w-full justify-center items-start'></div>
-                        <div className='flex flex-col w-full justify-end items-center mr-4'>   
+                        <div className='flex flex-col w-full justify-end items-start mr-14'>   
                             <Button color="primary" >Agregar nuevo tema</Button>
                         </div>
                     </div>
                 </div>
             </div>
-
-            <ComunidadPost className="w-1/2"/>
+            
+            <ComunidadPost className="w-full justify-start mx-5 px-2 "/>
+           
         </div>
   ) 
 }
--- src/components/ComunidadPost.js
+++ src/components/ComunidadPost.js
@@ -4,20 +4,19 @@ const ComunidadPost = ({className}) => {
     return (
       <div className={className}>
         <div className="flex flex-col w-full">
-          
           <div className="relative w-full  ">
-            <div className="flex flex-col w-full">
-              <div className="w-full px-4 mt-4">
-              <ComunidadFoto titulo={<strong>Diego</strong>} area={<strong>Neu</strong>} description="Encuesta sobre perdida del cabello"/>
+            <div className="flex flex-wrap">
+              <div className="w-5/12 px-4 mt-4">
+              <ComunidadFoto titulo={<strong>Grupo de Investigación en Neuropsicología</strong>} area={<strong>Neuropsicología</strong>} description="Este grupo se enfoca en la investigación y la aplicación clínica de la neuropsicología, explorando la relación entre el cerebro y el comportamiento. Los participantes discuten avances recientes en neurociencia cognitiva, comparten casos de estudio y colaboran en proyectos de investigación para avanzar en el campo."/>
               </div>  
-              <div className="w-full px-4 mt-4">
-              <ComunidadFoto nombre={<strong>Harry</strong>} area={<strong>Diego</strong>} description="pido opiniones sobre la depresion"/>
+              <div className="w-5/12 px-4 mt-4">
+              <ComunidadFoto titulo={<strong>Grupo de Apoyo para Profesionales en Salud Mental</strong>} area={<strong>Bienestar y Salud Mental</strong>} description="Este grupo ofrece un espacio seguro para que los doctores en psicología compartan experiencias, desafíos y estrategias de autocuidado. Los miembros brindan apoyo mutuo, comparten recursos sobre prevención del agotamiento profesional y técnicas para mantener un equilibrio saludable entre el trabajo y la vida personal."/>
               </div>
-              <div className="w-full px-4 mt-4">
-              <ComunidadFoto nombre={<strong>Tomas</strong>} area={<strong>Diego</strong>} description="Apoyo emocional para pacientes con enfermedades crónicas"/>
+              <div className="w-5/12 px-4 mt-4">
+              <ComunidadFoto titulo={<strong>Grupo de Psicoterapia Existencial y Humanista</strong>} area={<strong>Psicoterapia Existencial y Humanista</strong>} description="Este grupo se centra en la filosofía y práctica de la psicoterapia existencial y humanista, que enfatiza la exploración del sentido de la vida, la libertad y la responsabilidad personal. Los miembros comparten técnicas terapéuticas, reflexiones filosóficas y casos clínicos desde esta perspectiva."/>
               </div>
-              <div className="w-full px-4 mt-4">
-              <ComunidadFoto nombre={<strong>Carlos</strong>} area={<strong>Diego</strong>} description="La importancia del autocuidado en el manejo de enfermedades crónicas: Estrategias para mantener un equilibrio emocional"/>
+              <div className="w-5/12 px-4 mt-4">
+              <ComunidadFoto titulo={<strong>Grupo de Terapia Cognitivo-Conductual</strong>} area={<strong>Terapia Cognitivo-Conductual (TCC)</strong>} description="Este grupo está dedicado al estudio y la práctica de la terapia cognitivo-conductual, centrada en identificar y cambiar patrones de pensamiento y comportamiento que contribuyen a los problemas psicológicos. Los miembros comparten técnicas, casos clínicos y recursos para mejorar sus habilidades en TCC."/>
               </div>
             </div>  
           </div>
