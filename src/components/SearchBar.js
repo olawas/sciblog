d3cb9ce7b115164f874443c2a8b9b4d079ed361b
Jean-Code-J <71149487+Jean-Jimenez-Code@users.noreply.github.com>
2024-05-26 23:42:00 -0400

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,19 @@
-import {MagnifyingGlassIcon} from '@heroicons/react/24/solid'
-const SearchBar = ({className,placeholder}) => {
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+import Link from 'next/link'; // Importa Link de Next.js
+
+const SearchBar = ({ className }) => {
   return (
     <div className={className}>
       <div className='w-full h-10 flex flex-row'>
-        <input type='text' className=' w-full dark:text-gray-950 dark:bg-violet-100 py-5 px-3 inset-primary-bg outline outline-1 outline-violet-800 rounded-l-md'placeholder={placeholder}/>
-        <MagnifyingGlassIcon className='h-full text-on-primary p-1 bg-primary-bg outline outline-1 outline-violet-800 rounded-r-md '/>
+        <input type='text' placeholder='Busqueda'className='w-full dark:text-gray-950 dark:bg-violet-100 py-5 px-3 inset-primary-bg outline outline-1 outline-violet-800 rounded-l-md' />
+        <Link href="/buscar">
+          <button className='h-full bg-primary-bg outline outline-1 outline-violet-800 rounded-r-md cursor-pointer'>
+            <MagnifyingGlassIcon className='h-full text-on-primary p-1' />
+          </button>
+        </Link>
       </div>
     </div>
   )
 }
 
-export default SearchBar
+export default SearchBar;
