4b1fdd7af3b48be0cd862f466bbb955abd2fa3c5
Jean-Code-J <71149487+Jean-Jimenez-Code@users.noreply.github.com>
2024-06-11 12:12:54 -0400

    Cambio de modo claro en interfaz

--- src/app/agregarPublicacion/page.js
+++ src/app/agregarPublicacion/page.js
@@ -1,17 +1,17 @@
-"use client"
-import { useRouter } from 'next/navigation'; // Import useRouter from next/navigation
+"use client"// Importaciones necesarias
+import { useRouter } from 'next/navigation'; // Asegúrate de importar desde 'next/router'
 import { Input, Textarea, Button } from '@nextui-org/react';
 
 export default function Component() {
-  const router = useRouter(); // Initialize useRouter
+  const router = useRouter(); // Inicializa useRouter
 
   return (
-    <div className="flex h-full w-full items-center justify-center bg-[#3f3d56] px-4">
-      <div className="w-full max-w-md rounded-lg bg-white p-8 shadow-lg dark:bg-gray-950">
-        <h2 className="mb-4 text-2xl text-white font-bold">Nueva Publicación</h2>
+    <div className="flex h-full w-full items-center justify-center bg-background dark:bg-[#3f3d56] px-4">
+      <div className="w-full max-w-xl rounded-lg bg-white p-8 shadow-lg border border-gray-300 dark:bg-gray-950">
+        <h2 className="mb-4 text-2xl font-bold text-primary dark:text-white">Nueva Publicación</h2>
         <form>
-          <div>
-            <label className="mb-2 block text-sm font-medium text-gray-700 dark:text-gray-300" htmlFor="linkPowerBi">
+          <div className="mb-4"> {/* Agregado mb-4 para espacio debajo del Input */}
+            <label className="mb-2 block text-sm font-medium text-foreground dark:text-gray-300" htmlFor="linkPowerBi">
               Link Power BI
             </label>
             <Input
@@ -23,10 +23,11 @@ export default function Component() {
               placeholder="Ingresa el enlace de Power BI"
               id="linkPowerBi"
               type="text"
+              className="dark:text-gray-300 dark:placeholder-gray-500"
             />
           </div>
-          <div>
-            <label className="mb-2 block text-sm font-medium text-gray-700 dark:text-gray-300" htmlFor="description">
+          <div className="mb-4"> {/* Agregado mb-4 para espacio debajo del Textarea */}
+            <label className="mb-2 block text-sm font-medium text-foreground dark:text-gray-300" htmlFor="description">
               Añadir descripción
             </label>
             <Textarea
@@ -35,14 +36,15 @@ export default function Component() {
               color="primary"
               placeholder="Escribe una descripción aquí"
               id="description"
+              className="dark:text-gray-300 dark:placeholder-gray-500"
             />
           </div>
           <Button
-            className="w-full mb-2 mt-4 rounded-md bg-[#3f3d56] px-4 py-2 text-white hover:bg-[#2f2c44] focus:outline-none focus:ring-2 focus:ring-[#3f3d56] focus:ring-offset-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+            className="w-full mb-2 mt-4 rounded-md bg-primary dark:bg-gray-800 px-4 py-2 text-on-primary dark:text-white hover:bg-[#2f2c44] dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-primary dark:focus:ring-gray-800 focus:ring-offset-2"
             onClick={(e) => {
               e.preventDefault();
               console.log('El botón ha sido presionado');
-              router.push('/publicacion'); // Navigate to the desired route
+              router.push('/publicacion'); // Navega a la ruta deseada
             }}
           >
             Terminar publicación
