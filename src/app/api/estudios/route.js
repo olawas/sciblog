eb10c0ebf53078377a8319d2f32f92fc7010421b
Ignacio-Ruiz <ignacioeladio.ruiz@alumnos.ulagos>
2024-06-17 01:44:17 -0400

    Update tabla estudio
    Se agrega atributo enlace a tabla estudio  y se crea ruta API Dataset creando las consultas Post Y Get de esta misma

--- src/app/api/estudios/route.js
+++ src/app/api/estudios/route.js
@@ -1,17 +1,38 @@
 import Estudio from "@/services/Estudio"
 import { NextResponse } from "next/server" 
+import db from '@/libs/db';
 
-export const GET = async (req)  => {
+export const GET = async ()  => {
   try{
 
-    const {busqueda} = await req.json()
-    console.log('n9',busqueda)
-
-    const estudios = await Estudio.findMany(busqueda) //arreglo de coincidencias con busqueda
+    const estudios = await db.estudio.findMany() //arreglo de coincidencias con busqueda
 
     //const response = NextResponse.json({message: 'Estudios encontrados correctamente', data: estudio,}, {status: 200})
     //response.cookies.delete('next-auth.session-token')
-    return estudios //response
+    return NextResponse.json(estudios);  //response
+  }
+  catch(e){
+    console.error('SERVER ERROR', e)
+    return NextResponse.json({message: 'Error en el servidor'}, {status:500})
+  }
+}
+
+export const POST = async (req)  => {
+  try{
+
+    const nuevo_post = await req.json()
+    console.log('n9',nuevo_post)
+
+    const estudio = await db.estudio.create({
+      data: nuevo_post
+    })
+    if (!estudio){
+      return NextResponse.json({message: "ERROR AL CREAR ESTUDIO"},{status:400})
+    }
+
+    //const response = NextResponse.json({message: 'Estudios encontrados correctamente', data: estudio,}, {status: 200})
+    //response.cookies.delete('next-auth.session-token')
+    return NextResponse.json(estudio); //response
   }
   catch(e){
     console.error('SERVER ERROR', e)
